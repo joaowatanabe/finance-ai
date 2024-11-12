ecc027384c914d868ce4b8c216fd1d89886ae947
wabemusic <jvwatanabe@gmail.com>
2024-11-12 13:01:06 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,13 +1,13 @@
 import { auth } from "@clerk/nextjs/server";
+import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
-import NavBar from "../_components/navbar";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-  return <NavBar />;
+  return <Navbar />;
 };
 
 export default SubscriptionPage;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,9 +2,10 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -18,14 +19,16 @@ const TransactionsPage = async () => {
   });
   return (
     <>
-      <NavBar />
-      <div className="space-y-6 p-6">
-        {/* TITULO E BOTAO */}
+      <Navbar />
+      <div className="space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable columns={transactionColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
